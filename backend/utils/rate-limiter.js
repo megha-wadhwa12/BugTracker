6a83dfbe5f6009f87197288ba1c75048124ba9db
Megha Wadhwa <meghawadhwa20@gmail.com>
2026-02-10 15:53:23 +0530

    Changed rate-limit

--- backend/utils/rate-limiter.js
+++ backend/utils/rate-limiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 min
+  windowMs: 1 * 60 * 1000, // 15 min
   max: 10, // max 10 attempts
   message: "Too many attempts, try again later",
 });
